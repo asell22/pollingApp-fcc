bb15c9033be960cb89bd0c04da467d212257b924
asell22 <alexanderjsell@gmail.com>
2016-05-20 13:50:23 -0700

    userSchema methods added using crypto module

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var crypto = require('crypto');
+
 var userSchema = new Schema({
   username: {
     type: String,
@@ -10,6 +12,18 @@ var userSchema = new Schema({
   hash: String,
   salt: String
 })
+
+userSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 100000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 100000, 64).toString('hex');
+
+  return this.hash === hash;
+}
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
