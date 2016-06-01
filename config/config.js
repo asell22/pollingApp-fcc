577fdd3cc0f55db87d65dbebce7477a9d04ccdd6
asell22 <alexanderjsell@gmail.com>
2016-06-01 09:05:07 -0700

    removed config folder

--- config/config.js
+++
@@ -1,9 +0,0 @@
-var ids = {
-  twitter: {
-    consumerKey: '5lbloSnUgGCIWd6VDNetTwE9T',
-    consumeSecret: 'kKj246XLhaQwFGwXOjGX4GrPtV0y1watTTqcKuG54NwS4OkRMF',
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
-  }
-}
-
-module.exports = ids;
