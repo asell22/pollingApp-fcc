35d16fd48eaa962cb492c9457c14fe5e0b60b1bb
asell22 <alexanderjsell@gmail.com>
2016-06-23 09:04:28 -0700

    polls are listed in a table

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -1,7 +1,7 @@
 angular.module('voting')
 .controller('mainCtrl', function($scope, polls, $http) {
   var self = this;
-  self.title = "Awesome Voting App";
+  self.title = "Voting App";
   self.polls = polls.polls;
   $scope.deletePoll = function(id, index) {
     $http.delete('/polls/' + id).then(function(res){
@@ -9,5 +9,8 @@ angular.module('voting')
       self.polls.splice(index, 1);
       console.log("Deleted!");
     })
+  }
+  $scope.removeNavActiveClass = function() {
+    $('.nav li').removeClass('active');
   }
 })
