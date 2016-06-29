81d9e6ede2ae7ee652fb401af9b4f9677225f811
asell22 <alexanderjsell@gmail.com>
2016-06-28 18:19:51 -0700

    pull ip address from req.headers array

--- routes/index.js
+++ routes/index.js
@@ -81,7 +81,7 @@ router.param('poll', function(req, res, next, id) {
 });
 
 router.get('/polls/:poll', function(req, res) {
-  var ipAddr = req.header('x-forwarded-for');
+  var ipAddr = req.headers["x-forwarded-for"][0];
   // if (ipAddr){
   //   var list = ipAddr.split(",");
   //   ipAddr = list[list.length-1];
