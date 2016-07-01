de0152fb106e33fb7a719195c82492034ef52efb
asell22 <alexanderjsell@gmail.com>
2016-06-30 21:06:29 -0700

    trying to get first element of ip array

--- routes/index.js
+++ routes/index.js
@@ -81,7 +81,7 @@ router.param('poll', function(req, res, next, id) {
 });
 
 router.get('/polls/:poll', function(req, res) {
-  var ipAddr = req.ip;
+  var ipAddr = req.headers["x-forwarded-for"][0];
   // if (ipAddr){
   //   var list = ipAddr.split(",");
   //   ipAddr = list[list.length-1];
